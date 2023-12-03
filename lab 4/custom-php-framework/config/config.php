4e844e003d793d13007330d52e0dc8ae65ba0483
eliot264 <wk49439@zut.edu.pl>
2023-12-03 22:42:41 +0100

    finish

---
+++ lab 4/custom-php-framework/config/config.php
@@ -0,0 +1,5 @@
+<?php
+$config = [];
+$config['db_dsn'] = 'sqlite:C:\\Users\\kacpe\\Documents\\Studia\\Semestr 7\\Aplikacje internetowe\\lab 4\\custom-php-framework\\data.db';
+$config['db_user'] = '';
+$config['db_pass'] = '';
